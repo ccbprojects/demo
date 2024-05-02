4f792c5d529a9742fe22c57a634e361ef16ba0de
Julián Rincón <julian.rincon@mqa-bc.com>
2024-05-02 17:23:56 +0000

    Add kenny

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,7 +9,7 @@ public class DemoController {
     @GetMapping("/")
     public String hello() {
         
-        String page = "Hello Timmy y Butters y Cartman y Kyle y Stan!";    
+        String page = "Hello Timmy y Butters y Cartman y Kyle y Stan y Kenny!";    
         return String.format(page);
     }
 }
